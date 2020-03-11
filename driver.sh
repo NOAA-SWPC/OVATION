ea496ef90553d0bbc567192c1ea6334321ea2b36
Marcus E <marcus.england@noaa.gov>
2020-03-11 11:05:54 -0600

    Readying for staging

--- driver.sh
+++ driver.sh
@@ -1,7 +1,8 @@
 while true
 do
+  echo "Model and plot generation starting at: $(date)"
   cd /source
   python ovation_model.py && cd /source_plots && python ovation_plot.py
-  echo "Model and plot generation complete"
+  echo "Model and plot generation completed at: $(date)"
   sleep 600
 done
