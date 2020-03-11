eb0d201e063c464953d57e498366ef2fd8af88a6
Marcus England <marcus.england@noaa.gov>
2020-03-11 20:34:52 +0000

    Cleanup and README added

--- driver.sh
+++ driver.sh
@@ -4,6 +4,6 @@ do
   cd /source
   python ovation_model.py && cd /source_plots && python ovation_plot.py
   echo "Model and plot generation completed at: $(date)"
-  find /Output/* -mtime +5 -exec rm {} \;
+  find /Output/* -mtime +5 -type f -exec rm {} \;
   sleep 600
 done
